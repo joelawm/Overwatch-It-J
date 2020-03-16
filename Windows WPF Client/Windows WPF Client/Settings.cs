846224324484ba58b737a162895dfd3c20b1a291
Joe Meyer <joelawm@gmail.com>
2020-03-16 17:41:42 -0500

    Windows WPF Client Code
    -Added Settings class for preset notifications etc.
    -Added Toast Notfications for ZFS to start.

---
+++ Windows WPF Client/Windows WPF Client/Settings.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Windows_WPF_Client
+{
+    class Settings
+    {
+        //applcaition settings
+        public const string APP_NAME = "Monitor Applcation";
+
+
+        //ZFS Notifcation types
+        public const string ZFS_DEGRADED = "Degraded";
+        public const string ZFS_OFFLINE = "Offline";
+    }
+}
---
+++ Windows WPF Client/Windows WPF Client/ToastNotificationFunctions.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+//addd refrences for Toast
+using Windows.Data.Xml.Dom;
+using Windows.UI.Notifications;
+
+namespace Windows_WPF_Client
+{
+    class ToastNotificationFunctions
+    {
+        private const String APP_ID = "Monitor";
+
+        //notifications for ZFS
+        public static void ZFSNotification(string NotificationType)
+        {
+            //title of the popup
+            string title = "ZFS "+ NotificationType + " " + Settings.APP_NAME;
+            //datatime of the popup
+            string timeString = $"{DateTime.Now:HH:mm:ss}";
+            string message = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+            //string thomasImage = "https://www.thomasclaudiushuber.com/thomas.jpg";
+
+            string toastXmlString =
+            $@"<toast><visual>
+            <binding template='ToastGeneric'>
+            <text>{title}</text>
+            <text>{message}</text>
+            </binding>
+            </visual></toast>";
+
+            //create the xml notifciation
+            var xmlDoc = new XmlDocument();
+            xmlDoc.LoadXml(toastXmlString);
+
+            var toastNotification = new ToastNotification(xmlDoc);
+
+            var toastNotifier = ToastNotificationManager.CreateToastNotifier();
+            toastNotifier.Show(toastNotification);
+        }
+    }
+}
