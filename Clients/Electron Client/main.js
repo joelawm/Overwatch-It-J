e640a4296cfe79e2bb895e813333e9685be5b45c
jlmeyer <xdeltagix@gmail.com>
2020-05-15 01:31:33 -0500

    End of night changes
    -changed around the menu a bit

--- Clients/Electron Client/main.js
+++ Clients/Electron Client/main.js
@@ -1,14 +1,15 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, Menu } = require('electron')
+const isMac = process.platform === 'darwin'
 
 function createWindow () {
-  // Create the browser window.
-  const win = new BrowserWindow({width: 800,height: 600,webPreferences: {nodeIntegration: true}})
+	// Create the browser window.
+	const win = new BrowserWindow({width: 1200,height: 800,webPreferences: {nodeIntegration: true}})
 
-  // and load the index.html of the app.
-  win.loadFile('index.html')
+	// and load the index.html of the app.
+	win.loadFile('index.html')
 
-  // Open the DevTools.
-  win.webContents.openDevTools()
+	// Open the DevTools.
+	//win.webContents.openDevTools()
 }
 
 // This method will be called when Electron has finished
@@ -18,20 +19,143 @@ app.whenReady().then(createWindow)
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+	// On macOS it is common for applications and their menu bar
+	// to stay active until the user quits explicitly with Cmd + Q
+	if (process.platform !== 'darwin') {
+		app.quit()
+	}
 })
 
 app.on('activate', () => {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow()
-  }
+	// On macOS it's common to re-create a window in the app when the
+	// dock icon is clicked and there are no other windows open.
+	if (BrowserWindow.getAllWindows().length === 0) {
+		createWindow()
+	}
 })
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
+
+
+// Top Menu setup so far
+const template =
+[
+	// { role: 'appMenu' }
+	...(isMac ? [{
+		label: app.name,
+		submenu: [
+			{ role: 'about' },
+			{ type: 'separator' },
+			{ role: 'services' },
+			{ type: 'separator' },
+			{ role: 'hide' },
+			{ role: 'hideothers' },
+			{ role: 'unhide' },
+			{ type: 'separator' },
+			{ role: 'quit' }]
+		}] : []),
+		// { role: 'fileMenu' }
+		{
+			label: 'File',
+			submenu: [
+				isMac ? { role: 'close' } : { role: 'quit' }
+			]},
+			// { role: 'editMenu' }
+			{
+				label: 'Edit',
+				submenu: [
+					{ role: 'undo' },
+					{ role: 'redo' },
+					{ type: 'separator' },
+					{ role: 'cut' },
+					{ role: 'copy' },
+					{ role: 'paste' },
+					...(isMac ? [
+						{ role: 'pasteAndMatchStyle' },
+						{ role: 'delete' },
+						{ role: 'selectAll' },
+						{ type: 'separator' },
+						{
+							label: 'Speech',
+							submenu: [
+								{ role: 'startspeaking' },
+								{ role: 'stopspeaking' }
+							]
+						}
+					] : [
+						{ role: 'delete' },
+						{ type: 'separator' },
+						{ role: 'selectAll' }
+					])
+				]
+			},
+			// { role: 'viewMenu' }
+			{
+				label: 'View',
+				submenu: [
+					{ role: 'reload' },
+					{ role: 'forcereload' },
+					{ role: 'toggledevtools' },
+					{ type: 'separator' },
+					{ role: 'resetzoom' },
+					{ role: 'zoomin' },
+					{ role: 'zoomout' },
+					{ type: 'separator' },
+					{ role: 'togglefullscreen' }
+				]
+			},
+			// { role: 'windowMenu' }
+			{
+				label: 'Window',
+				submenu: [
+					{ role: 'minimize' },
+					{ role: 'zoom' },
+					...(isMac ? [
+						{ type: 'separator' },
+						{ role: 'front' },
+						{ type: 'separator' },
+						{ role: 'window' }
+					] : [
+						{ role: 'close' }
+					])
+				]
+			},
+			{
+				label: 'Scripts',
+				submenu: [
+					{ role: 'minimize' },
+					{ role: 'zoom' },
+					...(isMac ? [
+						{ type: 'separator' },
+						{ role: 'front' },
+						{ type: 'separator' },
+						{ role: 'window' }
+					] : [
+						{ role: 'close' }
+					])
+				]
+			},
+			{
+				role: 'help',
+				submenu: [
+					{
+						label: 'Learn More',
+						click: async () => {
+							const { shell } = require('electron')
+							await shell.openExternal('https://electronjs.org')
+						}
+					},
+					{
+						label: 'Website',
+						click: async () => {
+							const { shell } = require('electron')
+							await shell.openExternal('https://overwatchitj.io')
+						}
+					}
+				]
+			}
+		]
+
+const menu = Menu.buildFromTemplate(template)
+Menu.setApplicationMenu(menu)
